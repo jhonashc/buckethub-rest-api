d37d540e35c748ea17bf411f8e82ce0d23ef385f
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 17:23:40 -0500

    user module updated: create and update user dto added

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  firstName: string;
+
+  @IsString()
+  @MinLength(1)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(1)
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  avatarUrl?: string;
+}
