30f6646f8679bccf7de522cd617276dd77042950
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 17:22:21 -0500

    user module updated: user entity added

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,61 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+    name: 'first_name',
+  })
+  firstName: string;
+
+  @Column({
+    type: 'text',
+    name: 'last_name',
+  })
+  lastName: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'text',
+  })
+  password: string;
+
+  @Column({
+    type: 'text',
+    name: 'avatar_url',
+    nullable: true,
+  })
+  avatarUrl: string;
+
+  @Column({
+    type: 'boolean',
+    default: true,
+  })
+  status: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
