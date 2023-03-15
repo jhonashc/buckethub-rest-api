f645abbd878e5b97a0497295b17d489ab980be8d
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 17:26:19 -0500

    app module updated: global pipes for validations added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,9 +13,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix(globalPrefix);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(port, () => {
     Logger.log(`🚀 App running on port ${port}`);
-    Logger.log(`Running in ${environment}`);
+    Logger.log(`Running in ${environment.toUpperCase()}`);
   });
 }
 bootstrap();
