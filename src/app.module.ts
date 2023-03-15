78b7b3080b4698eacc6f9a72af468e63317d0c0e
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 15:30:36 -0500

    environments settings added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { CommonModule } from './common/common.module';
 
+import { JoiValidatonSchema } from './common/validators/joi.validator';
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: JoiValidatonSchema,
+    }),
+    UserModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
-  app.setGlobalPrefix('api');
+  const globalPrefix = 'api';
+  const port = config.get<number>('PORT');
+  const environment = config.get<string>('NODE_ENV');
 
-  await app.listen(3000);
+  app.setGlobalPrefix(globalPrefix);
+
+  await app.listen(port, () => {
+    Logger.log(`🚀 Server ready at http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`Running in ${environment}`);
+  });
 }
 bootstrap();
