f086fddad5b252ae653147b12b95e61e082a9d59
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 23:25:20 -0500

    repository module updated: repository entity added

--- src/repository/entities/repository.entity.ts
+++ src/repository/entities/repository.entity.ts
@@ -1,1 +1,74 @@
-export class Repository {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { UserEntity } from '../../user/entities/user.entity';
+
+export enum Status {
+  PENDING = 'pending',
+  DECLINED = 'declined',
+  ACCEPTED = 'accepted',
+  DELETED = 'deleted',
+}
+
+@Entity('repository')
+export class RepositoryEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  title: string;
+
+  @Column({
+    type: 'text',
+    unique: true,
+    nullable: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'text',
+  })
+  body: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.repositories, {
+    eager: true,
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'author_id' })
+  author: UserEntity;
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.PENDING,
+  })
+  status: Status;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
