de02a442411a7608c7ba485802518bd842a16bd4
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 23:26:55 -0500

    repository module updated: repository crud operations added

--- src/repository/repository.service.ts
+++ src/repository/repository.service.ts
@@ -1,26 +1,114 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRepositoryDto } from './dto/create-repository.dto';
-import { UpdateRepositoryDto } from './dto/update-repository.dto';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Any, In, Repository } from 'typeorm';
+
+import { CreateRepositoryDto, UpdateRepositoryDto } from './dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
+
+import { RepositoryEntity, Status } from './entities/repository.entity';
 
 @Injectable()
 export class RepositoryService {
-  create(createRepositoryDto: CreateRepositoryDto) {
-    return 'This action adds a new repository';
+  constructor(
+    @InjectRepository(RepositoryEntity)
+    private readonly repository: Repository<RepositoryEntity>,
+  ) {}
+
+  async createRepository(
+    createRepositoryDto: CreateRepositoryDto,
+  ): Promise<RepositoryEntity> {
+    const repositoryFound: RepositoryEntity = await this.repository.findOne({
+      where: {
+        title: createRepositoryDto.title,
+        status: Any([Status.PENDING, Status.ACCEPTED]),
+      },
+    });
+
+    if (repositoryFound) {
+      throw new ConflictException(
+        `The repository with title ${repositoryFound.title} already exits.`,
+      );
+    }
+
+    return this.repository.save(createRepositoryDto);
   }
 
-  findAll() {
-    return `This action returns all repository`;
+  getRepositories(paginationDto: PaginationDto): Promise<RepositoryEntity[]> {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.repository.find({
+      where: {
+        status: In([Status.PENDING, Status.ACCEPTED]),
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} repository`;
+  async getRepositoryById(id: string): Promise<RepositoryEntity> {
+    const repositoryFound: RepositoryEntity = await this.repository.findOne({
+      where: {
+        id,
+        status: Any([Status.PENDING, Status.ACCEPTED]),
+      },
+    });
+
+    if (!repositoryFound) {
+      throw new NotFoundException(
+        `The repository with id ${id} has not been found.`,
+      );
+    }
+
+    return repositoryFound;
   }
 
-  update(id: number, updateRepositoryDto: UpdateRepositoryDto) {
-    return `This action updates a #${id} repository`;
+  async updateRepositoryById(
+    id: string,
+    updateRepositoryDto: UpdateRepositoryDto,
+  ): Promise<RepositoryEntity> {
+    const repositoryFound: RepositoryEntity = await this.repository.findOne({
+      where: {
+        id,
+        status: In([Status.PENDING, Status.ACCEPTED, Status.DECLINED]),
+      },
+    });
+
+    if (!repositoryFound) {
+      throw new NotFoundException(
+        `The repository with id ${id} has not been found.`,
+      );
+    }
+
+    return this.repository.save({
+      ...repositoryFound,
+      ...updateRepositoryDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} repository`;
+  async deleteRepositoryById(id: string): Promise<RepositoryEntity> {
+    const repositoryFound: RepositoryEntity = await this.repository.findOne({
+      where: {
+        id,
+        status: In([Status.PENDING, Status.ACCEPTED, Status.DECLINED]),
+      },
+    });
+
+    if (!repositoryFound) {
+      throw new NotFoundException(
+        `The repository with id ${id} has not been found.`,
+      );
+    }
+
+    return this.repository.save({
+      ...repositoryFound,
+      status: Status.DELETED,
+    });
   }
 }
