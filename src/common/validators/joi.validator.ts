22684018b5c00d0e9416053357f017a8370dc63a
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-15 23:32:28 -0500

    joi validator updated: db environments added

--- src/common/validators/joi.validator.ts
+++ src/common/validators/joi.validator.ts
@@ -3,4 +3,9 @@ import * as Joi from 'joi';
 export const JoiValidatonSchema = Joi.object({
   NODE_ENV: Joi.string().valid('dev', 'prod').default('dev'),
   PORT: Joi.number().default(3000),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5432),
+  DB_NAME: Joi.string().default('postgres'),
+  DB_USERNAME: Joi.string().default('postgress'),
+  DB_PASSWORD: Joi.string().default('password'),
 });
